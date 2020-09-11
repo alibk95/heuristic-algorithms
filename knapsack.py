9051ea136e9660f36460193956e047608fbd76a1
alibk95 <abkarimi1995@gmail.com>
2020-09-11 15:24:05 +0200

    Bin Pack Problem file renamed knapsack problem file added

---
+++ knapsack.py
@@ -0,0 +1,20 @@
+class Knapsack():
+    # 0-1 Knapsack, items are unbreakable
+    def __init__(self):
+        pass
+
+    def solve(self, knapsack_volume, n, weight, value):
+
+        pass
+
+
+# Code driver
+item_value = [40, 80, 150]
+item_weight = [15, 30, 20]
+total_volume = 50
+n = len(item_value)
+# output must be equal to 230 [30, 20] <= 50
+
+
+kp = Knapsack()
+kp.solve(total_volume, n, item_weight, item_value)
